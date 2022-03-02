cf9be37abbf6b2aa4c043a7d2d2ee80a5e00590d
Jack-24265241 <24265241@edgehill.ac.uk>
2022-03-02 10:29:07 +0000

    Update ApiReader.java

--- JavaApiAccess/ApiAccess/src/ApiReader.java
+++ JavaApiAccess/ApiAccess/src/ApiReader.java
@@ -2,18 +2,13 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Authenticator;
-import java.net.InetSocketAddress;
-import java.net.ProxySelector;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpClient.Redirect;
-import java.net.http.HttpClient.Version;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.zip.GZIPInputStream;
 
@@ -21,20 +16,26 @@ public class ApiReader {
 
 	private static final HttpClient client = HttpClient.newHttpClient();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		// https://mkyong.com/java/how-to-send-http-request-getpost-in-java/
 		HttpRequest request = HttpRequest.newBuilder()
 				.GET()
 				.header("accept", "application/json")
-		        .uri(URI.create("https://api.coronavirus.data.gov.uk/v1/data"))
+		        .uri(URI.create("https://api.coronavirus.data.gov.uk/v1/data?"
+		        		+ "filters=areaType=nation;areaName=england"
+		        		+ "&structure=" + URLEncoder.encode("{\"date\":\"date\",\"name\":\"areaName\",\"dailyCases\":\"newCasesByPublishDate\",\"cumulativeCases\":\"cumCasesByPublishDate\", \"60DayDeath\":\"cumDeaths60DaysByPublishDate\" ,\"dailyDeaths\":\"newDeaths28DaysByPublishDate\",\"cumulativeDeaths\":\"cumDeaths28DaysByPublishDate\"}", "UTF-8")
+		        		//+ "&latestBy=newCasesByPublishDate"
+		        		))
 		        .timeout(Duration.ofMinutes(2))
 		        .build();
 		
 		HttpResponse<byte[]> response;
 		try {
 			response = client.send(request, BodyHandlers.ofByteArray());
-			//System.out.println(response.statusCode());
-			System.out.println(decompress(response.body())); 
+			System.out.println(response.statusCode());
+			String decodedResponse = decompress(response.body());
+			String dataString = decodedResponse.substring(decodedResponse.indexOf('[') + 1, decodedResponse.indexOf(']'));
+			System.out.println(dataString);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
